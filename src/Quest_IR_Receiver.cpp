cd91388298241ca174272f79dcb91a56df1e55bf
Aaron Silinskas <aaron@mindwidgets.com>
2018-11-25 22:17:44 -0800

    initial commit, receives IR and dumps it, but no decoding yet

---
+++ src/Quest_IR_Receiver.cpp
@@ -0,0 +1,76 @@
+#include "Quest_IR_Receiver.h"
+
+void Quest_IR_Receiver::begin(uint8_t irPin)
+{
+    receiver = IRrecvPCI(irPin);
+    receiver.enableIRIn();
+}
+
+bool Quest_IR_Receiver::hasData()
+{
+    return receiver.getResults();
+}
+
+void Quest_IR_Receiver::reset()
+{
+    receiver.enableIRIn();
+}
+
+bool Quest_IR_Receiver::decodeData()
+{
+    if (!checkBuffer(1, 2600) || !checkBuffer(2, 500))
+    {
+        return false;
+    }
+
+    //   uint8_t data[MAX_IR_DATA_LENGTH];
+    //   uint8_t dataLength;
+
+    return false;
+}
+
+void Quest_IR_Receiver::dumpData()
+{
+    Serial.print(F("Header: "));
+    Serial.print(recvGlobal.recvBuffer[1], DEC);
+    Serial.print(F(", "));
+    Serial.println(recvGlobal.recvBuffer[2], DEC);
+
+    int bitCount = 0;
+    int currentBits = 0;
+    for (bufIndex_t i = 3; i < recvGlobal.recvLength; i += 2)
+    {
+        int mark = recvGlobal.recvBuffer[i];
+        int bitDelay = recvGlobal.recvBuffer[i + 1];
+
+        Serial.print(F(" Mark "));
+        Serial.print(mark, DEC);
+        Serial.print(F(" Delay "));
+        Serial.println(bitDelay, DEC);
+
+        currentBits = currentBits << 1;
+        if (bitDelay > 900)
+        {
+            currentBits |= 1;
+        }
+        bitCount++;
+        if (bitCount == 8)
+        {
+            Serial.print(F("HEX CHAR = "));
+            Serial.println(currentBits, HEX);
+            bitCount = 0;
+            currentBits = 0;
+        }
+    }
+    Serial.println();
+}
+
+bool Quest_IR_Receiver::checkBuffer(bufIndex_t index, uint16_t expected)
+{
+    if (index >= recvGlobal.recvLength)
+    {
+        return false;
+    }
+    uint16_t actual = recvGlobal.recvBuffer[index];
+    return abs(actual - expected) < 100;
+}
---
+++ src/Quest_IR_Receiver.h
@@ -0,0 +1,25 @@
+#ifndef quest_ir_receiver_h
+#define quest_ir_receiver_h
+
+#include <IRLibAll.h>
+
+#define MAX_IR_DATA_LENGTH 48
+
+class Quest_IR_Receiver
+{
+public:
+  uint8_t data[MAX_IR_DATA_LENGTH];
+  uint8_t dataLength;
+
+  void begin(uint8_t irPin);
+  bool hasData();
+  bool decodeData();
+  void reset();
+  void dumpData();
+
+private:
+  IRrecvPCI receiver;
+  bool checkBuffer(bufIndex_t index, uint16_t expected);
+};
+
+#endif
